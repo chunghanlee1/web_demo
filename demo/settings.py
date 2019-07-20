46d142ff12f163fc07f465ecb5e04c8ffe686e7e
chunghanlee1 <chunghan.lee1@gmail.com>
2019-07-20 10:07:18 +0800

    add ignore

--- demo/settings.py
+++ demo/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
-
+from demo.secret import SECRET_KEY
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 TEMPLATE_DIR=os.path.join(BASE_DIR, 'templates')
@@ -47,6 +47,8 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+SECRET_KEY = SECRET_KEY
 
 ROOT_URLCONF = 'demo.urls'
 
